0dba9f0191e92e0d830081d0b83fddec11330556
GSkyler <gaoskyler@gmail.com>
2019-11-16 18:42:24 -0600

    Get data from firebase; get messages & group name from database

--- app/src/main/java/com/example/groupeventplanner/MainActivity.java
+++ app/src/main/java/com/example/groupeventplanner/MainActivity.java
@@ -1,14 +1,104 @@
 package com.example.groupeventplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private TextView groupNameTextView;
+    private ListView messageListView;
+    private ArrayList<String> messages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messages = new ArrayList<>();
+
+        groupNameTextView = findViewById(R.id.groupNameTextView);
+        messageListView = findViewById(R.id.messageListView);
+
+        updateGroupName();
+        updateMessages();
+        updateMessageListView();
+
     }
+
+    public void updateGroupName(){
+        db.collection("groups").document("Example Group 1")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if(document.exists()){
+                                Map<String, Object> data = document.getData();
+                                String groupName = (String)data.get("name");
+                                groupNameTextView.setText(groupName);
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void updateMessages(){
+//  change document from Example Group 1 to a document parameter passed in from previous menus
+        db.collection("groups").document("Example Group 1").collection("People")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful() && task.getResult() != null) {
+                            for(QueryDocumentSnapshot document: task.getResult()){
+                                Map<String, Object> data = document.getData();
+                                messages.add(data.get("name") + ": " + data.get("message"));
+                            }
+                            updateMessageListView();
+                        }
+                    }
+                });
+    }
+
+    public void updateMessageListView(){
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.msgtextview, messages);
+        messageListView.setAdapter(arrayAdapter);
+    }
+
 }
