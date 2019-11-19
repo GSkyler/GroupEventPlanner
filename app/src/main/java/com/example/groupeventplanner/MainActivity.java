599e3436363ffc9f5180c573953d50abc724dbf9
GSkyler <gaoskyler@gmail.com>
2019-11-18 21:00:04 -0600

    Changed color of user's text

--- app/src/main/java/com/example/groupeventplanner/MainActivity.java
+++ app/src/main/java/com/example/groupeventplanner/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.View.OnKeyListener;
 import android.view.View;
 import android.view.KeyEvent;
