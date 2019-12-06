0c7a55a17f77348fb185aa179feea4d1c301d069
GSkyler <gaoskyler@gmail.com>
2019-12-06 14:34:02 -0600

    added login and register buttons; logging in, creating new user not working

--- app/src/main/java/com/example/groupeventplanner/UserLogin.java
+++ app/src/main/java/com/example/groupeventplanner/UserLogin.java
@@ -3,8 +3,11 @@ package com.example.groupeventplanner;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,6 +17,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class UserLogin extends AppCompatActivity {
@@ -21,6 +26,9 @@ public class UserLogin extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private EditText usernameEditText;
     private TextView textView;
+    private String username;
+//  REMOVE GROUPNAME; MAKE NEW ACTIVITY OF GROUP LISTS
+    private String groupName = "Example Group 1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,35 +37,92 @@ public class UserLogin extends AppCompatActivity {
 
         usernameEditText = findViewById(R.id.usernameEditText);
         textView = findViewById(R.id.textView);
+        usernameEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getAction() == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER){
+                    try  {
+                        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                    } catch (Exception e) {
+
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void signIn(View view){
-
+        checkUsernameDatabase(usernameEditText.toString(), true);
+    }
+    public void register(View view){
+        checkUsernameDatabase(usernameEditText.toString(), false);
     }
 
-    public void checkDuplicateUsername(String username){
+    public void checkUsernameDatabase(final String name, final boolean signIn){
         db.collection("users")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if(task.isSuccessful() && task.getResult() != null) {
-//                            messages.clear();
+                            boolean foundName = false;
                             for(QueryDocumentSnapshot document: task.getResult()){
                                 Map<String, Object> data = document.getData();
-//                                if(!(data.get("name").equals(username))) {
-//                                    messages.add(data.get("name") + ": " + data.get("message"));
-//                                }
-//                                else{
-//                                    String username = (String)data.get("name") + ":";
-//                                    usernameTextView.setText(username);
-//                                    messageEditText.setText((String)data.get("message"));
-//                                }
+                                System.out.println(data.get("name"));
+                                if(data.get("name").equals(name)){
+                                    System.out.println("name matches input");
+                                    if(signIn){
+                                        System.out.println("logging in");
+                                        username = (String)data.get("name");
+                                        foundName = true;
+                                        goToGroup();
+                                    }
+                                    else{
+                                        System.out.println("duplicate username");
+                                        textView.setText("Username already taken!");
+                                        foundName = true;
+                                    }
+                                }
+                                if(foundName){
+                                    return;
+                                }
                             }
-//                            updateMessageListView();
+                            if(!foundName){
+                                if(signIn){
+                                    System.out.println("incorrect username");
+                                    textView.setText("Username is incorrect");
+                                }
+                                else{
+                                    System.out.println("creating new user");
+                                    username = name;
+                                    System.out.println("set username, creating newUser map");
+                                    Map<String, Object> newUser = new HashMap<>();
+                                    newUser.put("name", name);
+                                    newUser.put("id", 77777);
+                                    newUser.put("groups", Arrays.asList("Example Group 1"));
+                                    System.out.println("calling addUser");
+                                    addUser(newUser);
+                                    goToGroup();
+                                }
+                            }
                         }
                     }
                 });
+    }
+
+    public void goToGroup(){
+        Intent myintent = new Intent(this, MainActivity.class);
+        String[] data = {username, groupName};
+        myintent.putExtra("data", data);
+        startActivity(myintent);
+    }
+
+    public void addUser(Map<String, Object> newUser){
+        System.out.println("in addUser, " + (String)newUser.get("name"));
+        db.collection("users").document((String)newUser.get("name")).set(newUser);
     }
 
 }
